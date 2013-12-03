e1c156d1b29637553e4acbf120f16d13a5e16848
Lucas Simon <lucassrod@gmail.com>
2013-12-03 07:19:21 -0200

    Faz o include da chamada da app api

--- pywatch/urls.py
+++ pywatch/urls.py
@@ -1,17 +1,25 @@
 from django.conf.urls import patterns, include, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pywatch.views.home', name='home'),
-    # url(r'^pywatch/', include('pywatch.foo.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^api/v1/', include('api.urls')),
+)
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'api'])
+
+# Default login/logout views
+urlpatterns += patterns(
+    '',
+    url(
+        r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework'),
+    ),
 )
