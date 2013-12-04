e9b0c8902f46ff96085ed4821ed0e6efe58f6c4f
Lucas Simon <lucassrod@gmail.com>
2013-12-03 23:15:58 -0200

    Cria o model de palestras e medias

--- talks/models.py
+++ talks/models.py
@@ -1,3 +1,36 @@
 # -*- coding:utf-8 -*-
 
 # Core Django imports
+from django.utils.translation import ugettext as _
+
+# Realative imports of the 'app-name' package
+from core.models import TimeStampedModel, StandardItemStuffModel,\
+    Media
+
+
+class Talk(TimeStampedModel, StandardItemStuffModel):
+    """
+    Model responsavel pelos palestrante
+    """
+    class Meta:
+        ordering = ['created']
+        verbose_name = _(u'Palestrante')
+        verbose_name_plural = _(u'Palestrantes')
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+
+class MediaTalk(Media):
+    """
+    Model responsavel por criar as medias
+    de palestrantes
+    """
+
+    class Meta:
+        ordering = ['created']
+        verbose_name = _(u'Media Palestrante')
+        verbose_name_plural = _(u'Medias Palestrantes')
+
+    def __unicode__(self):
+        return u'%s - %s' % (self.name, self.url)
