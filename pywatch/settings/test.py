8b4c6cd17a000595aad281da4b0d6da1a54d353a
Lucas Simon <lucassrod@gmail.com>
2013-12-04 22:46:01 -0200

    Configura o Django Nose no settings de test

--- pywatch/settings/test.py
+++ pywatch/settings/test.py
@@ -1,10 +1,15 @@
+import os
 from base import *
+from decouple import Config
+import dj_database_url
 
 
+config = Config(PROJECT_DIR.child('confs')+'/settings.ini')
 ########## INSTALLED APPS CONFIGURATION
 
 INSTALLED_APPS += (
     'django_nose',
+    'nose',
 )
 
 ########## TEST SETTINGS
@@ -12,6 +17,8 @@ TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
 TEST_DISCOVER_TOP_LEVEL = PROJECT_DIR
 TEST_DISCOVER_ROOT = PROJECT_DIR
 TEST_DISCOVER_PATTERN = "test_*"
+NOSE_ARGS = ['--verbosity=2', '-x', '-d']
+os.environ['REUSE_DB'] = "1"
 ########## END TEST SETTINGS
 
 ########## DEBUG CONFIGURATION
