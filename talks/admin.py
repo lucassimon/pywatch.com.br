f9bb9cc0d3fdc49442f611bc1a0f6a975839a281
Lucas Simon <lucassrod@gmail.com>
2013-12-04 22:47:46 -0200

    Cria o admin da app talk

--- talks/admin.py
+++ talks/admin.py
@@ -1,7 +1,41 @@
 # -*- coding:utf-8 -*-
 
 # Core Django imports
+from django.contrib import admin
 
 # Third-party app imports
 
 # Realative imports of the 'app-name' package
+from .models import Talk, MediaTalk
+
+
+class MediaInline(admin.TabularInline):
+    """
+    Formulario de media em linha
+    """
+    model = MediaTalk
+    extra = 2
+
+
+class TalkAdmin(admin.ModelAdmin):
+    """
+    Classe admin utilizada no django admin para oferecer as
+    opcoes de CRUD do model Talk
+    """
+
+    inlines = [MediaInline, ]
+
+    # campos a serem exibidos na tabela
+    list_display = (
+        'speaker', 'title', 'summary',
+        'created'
+    )
+
+    date_hierarchy = 'created'
+
+    # campos que utilizam buscas no model
+    search_fields = ('title', 'created', )
+
+    list_filter = ('created', )
+
+admin.site.register(Talk, TalkAdmin)
