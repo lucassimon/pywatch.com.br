71431a2907d809e5ef775e9697f93860b8519ece
Lucas Simon <lucassrod@gmail.com>
2013-12-22 00:02:24 -0200

    Corrige as configuracoes de test do staging

--- pywatch/settings/staging.py
+++ pywatch/settings/staging.py
@@ -1,19 +1,25 @@
+import os
 from base import *
+from decouple import ConfigIni
+import dj_database_url
+
+
+config = ConfigIni(PROJECT_DIR.child('confs')+'/settings.ini')
+########## INSTALLED APPS CONFIGURATION
+
+INSTALLED_APPS += (
+    'django_nose',
+    'nose',
+)
+
 ########## TEST SETTINGS
-TEST_RUNNER = "discover_runner.DiscoverRunner"
+TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
 TEST_DISCOVER_TOP_LEVEL = PROJECT_DIR
 TEST_DISCOVER_ROOT = PROJECT_DIR
 TEST_DISCOVER_PATTERN = "test_*"
+NOSE_ARGS = ['--verbosity=2', '-x', '-d']
+os.environ['REUSE_DB'] = "1"
 ########## END TEST SETTINGS
-
-########## DEBUG CONFIGURATION
-DEBUG = True
-
-TEMPLATE_DEBUG = DEBUG
-
-COMPRESS_ENABLED = not DEBUG
-########## END DEBUG CONFIGURATION
-
 
 ########## DATABASE CONFIGURATION
 DATABASES = {
