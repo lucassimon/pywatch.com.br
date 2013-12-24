07fad2abd31273c0240c3380b6f11e904c17c239
Lucas Simon <lucassrod@gmail.com>
2013-12-24 16:59:42 -0200

    Cria uma function view central da api rest
    Criado uma funcao para exibir os endpoints utilizados na api rest

--- api/views.py
+++ api/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+# -*- coding:utf-8 -*-
+
+# Stdlib imports
+
+# Core Django imports
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+# Third-party app imports
+
+# Imports from your apps
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    """
+    Endpoint de toda a API REST
+    """
+
+    return Response(
+        {
+            'speakers': reverse('speaker-list', request=request),
+        }
+    )
