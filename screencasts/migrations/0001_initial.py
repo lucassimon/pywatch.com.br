bc199143407967f8691665c06f9638043cbcae39
Lucas Simon <lucassrod@gmail.com>
2014-04-28 21:21:26 -0300

    Cria as migracoes da app de screencasts

---
+++ screencasts/migrations/0001_initial.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Screencast'
+        db.create_table(u'screencasts_screencast', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('speaker', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['speakers.Speaker'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, unique=True, null=True)),
+            ('summary', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'screencasts', ['Screencast'])
+
+        # Adding model 'MediaScreencast'
+        db.create_table(u'screencasts_mediascreencast', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('type', self.gf('django.db.models.fields.CharField')(max_length=3)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
+            ('screencast', self.gf('django.db.models.fields.related.ForeignKey')(related_name='medias', to=orm['screencasts.Screencast'])),
+        ))
+        db.send_create_signal(u'screencasts', ['MediaScreencast'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Screencast'
+        db.delete_table(u'screencasts_screencast')
+
+        # Deleting model 'MediaScreencast'
+        db.delete_table(u'screencasts_mediascreencast')
+
+
+    models = {
+        u'screencasts.mediascreencast': {
+            'Meta': {'ordering': "['created']", 'object_name': 'MediaScreencast'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'screencast': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'medias'", 'to': u"orm['screencasts.Screencast']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        u'screencasts.screencast': {
+            'Meta': {'ordering': "['created']", 'object_name': 'Screencast'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True'}),
+            'speaker': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['speakers.Speaker']"}),
+            'summary': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'speakers.speaker': {
+            'Meta': {'ordering': "['created']", 'object_name': 'Speaker'},
+            'bio': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['screencasts']
