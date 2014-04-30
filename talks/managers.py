478ac702f8733bf6c85a90cac70155c83f205ca5
Lucas Simon <lucassrod@gmail.com>
2014-04-30 20:46:32 -0300

    Altera o manager de palestras

--- talks/managers.py
+++ talks/managers.py
@@ -3,12 +3,26 @@
 # Core Django imports
 from django.db import models
 
+# Relative imports of the 'app-name' package
 
-class TalkMostRecentCreatedManager(models.Manager):
+
+class TalkQueryset(models.query.QuerySet):
     """
-    Manager responsavel por trazer os
-    palestras criados recentemente
+    Classe para definir os querysets do model palestras
     """
+    def latest_with_limits(self, l):
+        """
+        :param l: Número para limitar a busca
+        """
+        return self.order_by("-created")[:l]
+
+
+class TalkManager(models.Manager):
+    """
+    Define um manager para o model de palestras
+    """
+    def get_queryset(self):
+        return TalkQueryset(self.model, using=self._db)
 
     def latest_with_limits(self, limit):
         """
@@ -16,5 +30,4 @@ class TalkMostRecentCreatedManager(models.Manager):
         de acordo com o limit setado
         :param limit: Integer seta o limite da busca
         """
-        self.limit = limit
-        return self.order_by("-created")[:self.limit]
+        return self.get_queryset().latest_with_limits(limit)
