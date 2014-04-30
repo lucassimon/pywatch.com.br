19f1f8efeb7310995ce33c13e565fa02a9340939
Lucas Simon <lucassrod@gmail.com>
2014-04-30 16:58:20 -0300

    Cria o model serie

--- screencasts/models.py
+++ screencasts/models.py
@@ -5,15 +5,77 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse
 
+# Thirdy Apps imports
+from mptt.models import MPTTModel, TreeForeignKey
+
 # Realative imports of the 'app-name' package
 from core.models import TimeStampedModel, StandardItemStuffModel,\
     Media
+
+
+class Serie(TimeStampedModel):
+    """
+    Model reponsável por cadastrar as series
+    e podem conter vários screencasts.
+    """
+
+    name = models.CharField(
+        verbose_name=_(u'Nome'),
+        max_length=255
+    )
+    """
+    Atributo da classe  para identificar um evento
+    e seu nome
+
+    Caracteristicas:
+    CharField
+    verbose name: Nome
+    max length: 255
+    """
+
+    slug = models.SlugField(
+        verbose_name=_(u'Slug'),
+        unique=True,
+        null=True
+    )
+    """
+    Atributo da classe para criar um slug baseado
+    no nome do evento
+
+    Caracteristicas:
+    SlugField
+    verbose name: Slug
+    max length: 255
+    """
+
+    class Meta:
+        ordering = ['created']
+        verbose_name = _(u'Serie')
+        verbose_name_plural = _(u'Series')
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
 
 
 class Screencast(TimeStampedModel, StandardItemStuffModel):
     """
     Model responsavel pelos screencasts gravados
     pela comunidade
+    """
+
+    serie = models.ForeignKey(
+        Serie,
+        verbose_name=_(u'Serie'),
+        blank=True,
+        null=True
+    )
+    """
+    Atributo da classe Talk para
+    referenciar um objeto da classe Event
+
+    Caracteristicas:
+    ForeignKey
+    verbose name: Evento
     """
 
     def get_absolute_url(self):
