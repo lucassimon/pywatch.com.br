aa70a1e701dc309b9016e0549caac5eb2a252917
Lucas Simon <lucassrod@gmail.com>
2014-04-30 16:57:19 -0300

    Cria o model evento e associa a palestra

--- talks/models.py
+++ talks/models.py
@@ -5,15 +5,78 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse
 
+# Thirdy Apps imports
+from mptt.models import MPTTModel, TreeForeignKey
+
 # Realative imports of the 'app-name' package
 from core.models import TimeStampedModel, StandardItemStuffModel,\
     Media
 from .managers import TalkMostRecentCreatedManager
 
 
+class Event(TimeStampedModel):
+    """
+    Model reponsável por cadastrar os eventos
+    e podem conter várias palestras.
+    """
+
+    name = models.CharField(
+        verbose_name=_(u'Nome'),
+        max_length=255
+    )
+    """
+    Atributo da classe  para identificar um evento
+    e seu nome
+
+    Caracteristicas:
+    CharField
+    verbose name: Nome
+    max length: 255
+    """
+
+    slug = models.SlugField(
+        verbose_name=_(u'Slug'),
+        unique=True,
+        null=True
+    )
+    """
+    Atributo da classe para criar um slug baseado
+    no nome do evento
+
+    Caracteristicas:
+    SlugField
+    verbose name: Slug
+    max length: 255
+    """
+
+    class Meta:
+        ordering = ['created']
+        verbose_name = _(u'Evento')
+        verbose_name_plural = _(u'Eventos')
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+
 class Talk(TimeStampedModel, StandardItemStuffModel):
     """
-    Model responsavel pelos palestrante
+    Model responsavel pelos palestras
+    e pode ser associada a um evento
+    """
+
+    event = models.ForeignKey(
+        Event,
+        verbose_name=_(u'Evento'),
+        blank=True,
+        null=True
+    )
+    """
+    Atributo da classe Talk para
+    referenciar um objeto da classe Event
+
+    Caracteristicas:
+    ForeignKey
+    verbose name: Evento
     """
 
     objects = TalkMostRecentCreatedManager()
