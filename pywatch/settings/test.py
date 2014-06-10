25715e259d412ded711b2e380b1f91aab4d94e1f
Lucas Simon <lucassrod@gmail.com>
2014-06-10 20:39:21 -0300

    Corrige configuracao de migracao do taggit

--- pywatch/settings/test.py
+++ pywatch/settings/test.py
@@ -5,15 +5,14 @@ import dj_database_url
 
 
 config = ConfigIni(PROJECT_DIR.child('confs')+'/settings.ini')
-########## INSTALLED APPS CONFIGURATION
+# ######## INSTALLED APPS CONFIGURATION
 
 INSTALLED_APPS += (
     'django_nose',
     'nose',
 )
 
-########## TEST SETTINGS
-os.environ['REUSE_DB'] = "1"
+# ######### TEST SETTINGS
 TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
 TEST_DISCOVER_TOP_LEVEL = PROJECT_DIR
 TEST_DISCOVER_ROOT = PROJECT_DIR
@@ -26,21 +25,25 @@ NOSE_ARGS = [
     '--with-xtraceback',
     '--with-progressive',
 ]
-########## END TEST SETTINGS
+# ######### END TEST SETTINGS
 
-########## DEBUG CONFIGURATION
+# ######### DEBUG CONFIGURATION
 DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
 
 COMPRESS_ENABLED = not DEBUG
-########## END DEBUG CONFIGURATION
+# ######### END DEBUG CONFIGURATION
 
 
-########## DATABASE CONFIGURATION
+# ######### DATABASE CONFIGURATION
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_TEST_URL')
     )
 }
-########## END DATABASE CONFIGURATION
+# ########## END DATABASE CONFIGURATION
+
+SOUTH_MIGRATION_MODULES = {
+    'taggit': 'taggit.south_migrations'
+}
