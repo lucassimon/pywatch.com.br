331baf6871df1145db4339553eaf2eb54ab9565a
Lucas Simon <lucassrod@gmail.com>
2014-06-22 20:30:13 -0300

    Cria o import do dashboard template view

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -1,9 +1,16 @@
 # -*- coding:utf-8 -*-
 
+# Stdlib imports
+
+# Core Django imports
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import patterns, url, include
 from django.core.urlresolvers import reverse_lazy
 from django.conf import settings
+# Third-party app imports
+
+# Imports from your apps
+from .views import DashboardIndexTemplateView
 
 urlpatterns = patterns(
     'dashboards.views',
