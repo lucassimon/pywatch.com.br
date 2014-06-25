8e9c316077c090404ab8bf9835b2df101679faff
Lucas Simon <lucassrod@gmail.com>
2014-06-25 13:07:07 -0300

    Cria as views de templates e listagem de palestras

--- dashboards/views.py
+++ dashboards/views.py
@@ -6,6 +6,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 # Third-party app imports
 
@@ -50,3 +51,65 @@ class DashboardIndexTemplateView(TemplateView):
         #     ).count()
         # )
         return context
+
+
+class TalkIndexTemplateView(TemplateView):
+    """
+    Classe generica para renderizar o template
+    da index de palestras no dashboard
+    """
+
+    template_name = "dashboards/talks/index.html"
+    """
+    Define o nome do template a ser utilizado
+    """
+
+
+class TalkListView(ListView):
+    """
+    Classe generica para listar as palestras
+    do usuário
+    """
+
+    template_name = "dashboards/talks/list.html"
+    """
+    Define o nome do template a ser utilizado
+    """
+    model = Talk
+
+    context_object_name = "talk_list"
+    """
+    Define o nome do objeto a ser renderizado
+    no contexto da requisição
+    """
+
+    paginate_by = 10
+    """
+    Define o tamanho dos items a serem
+    paginados
+    """
+
+    def get_context_data(self, **kwargs):
+        """
+        Seta objetos para o contexto
+        """
+        context = super(
+            TalkListView, self
+        ).get_context_data(**kwargs)
+        context['talk_list'] = Talk.objects.filter(
+            speaker=context.get('view').request.user
+        )
+
+        return context
+
+
+class ScreencastIndexTemplateView(TemplateView):
+    """
+    Classe generica para renderizar o template
+    da index de palestras no dashboard
+    """
+
+    template_name = "dashboards/screencasts/index.html"
+    """
+    Define o nome do template a ser utilizado
+    """
