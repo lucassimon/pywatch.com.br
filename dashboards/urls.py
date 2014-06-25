27ba0f023f4deee033c2b49f5926c262d970e3bf
Lucas Simon <lucassrod@gmail.com>
2014-06-25 13:07:24 -0300

    Cria as urls para palestras

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -10,13 +10,33 @@ from django.conf import settings
 # Third-party app imports
 
 # Imports from your apps
-from .views import DashboardIndexTemplateView
+from .views import (
+    DashboardIndexTemplateView,
+    TalkIndexTemplateView,
+    TalkListView,
+    ScreencastIndexTemplateView,
+)
 
 urlpatterns = patterns(
     'dashboards.views',
     url(
         r'^$',
-        login_required(DashboardIndexTemplateView.as_view()),
+        DashboardIndexTemplateView.as_view(),
         name='dashboard-index-view'
+    ),
+    url(
+        r'^talks/$',
+        TalkIndexTemplateView.as_view(),
+        name='dashboard-talk-index-view'
+    ),
+    url(
+        r'^talks/list/$',
+        TalkListView.as_view(),
+        name='dashboard-talk-list-view'
+    ),
+    url(
+        r'^screencasts/$',
+        ScreencastIndexTemplateView.as_view(),
+        name='dashboard-screencast-index-view'
     ),
 )
