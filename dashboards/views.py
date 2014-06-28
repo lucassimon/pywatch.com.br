c9acb849d393dfbfbb6aa79c7aa32a0524c34a35
Lucas Simon <lucassrod@gmail.com>
2014-06-27 22:16:08 -0300

    Cria a classe generica para criar uma palestra

--- dashboards/views.py
+++ dashboards/views.py
@@ -3,15 +3,18 @@
 # Stdlib imports
 
 # Core Django imports
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.http import HttpResponseRedirect
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse_lazy
 
 # Third-party app imports
 
 # Imports from your apps
+from speakers.models import SpeakerUser
 from talks.models import Talk
+from talks.forms import TalkForm
 from screencasts.models import Screencast
 
 
@@ -63,6 +66,70 @@ class TalkIndexTemplateView(TemplateView):
     """
     Define o nome do template a ser utilizado
     """
+
+
+class TalkCreateView(CreateView):
+    """
+    Classe generica para criar as palestras
+    do usuário
+    """
+    template_name = "dashboards/talks/create.html"
+    """
+    Define o nome do template a ser utilizado
+    """
+
+    form_class = TalkForm
+    """
+    Define o formulario a ser renderizado no template
+    """
+
+    success_url = reverse_lazy('dashboards:dashboard-talk-list-view')
+    """
+    Define a url de sucesso apos criar o objeto de palestra
+    """
+
+    def form_valid(self, form):
+        """
+        Sobrescreve o metodo form_valid para buscar
+        o palestrante/user através do request
+        """
+        talk = form.save(commit=False)
+        # seta o commit do formulario como false
+
+        from uuslug import slugify
+        # importa o slugfy
+
+        talk.slug = slugify(talk.title)
+        # adiciona no formulario talk
+        # no atributo slug a chamada do metodo
+        # slugify de acordo com o titulo
+        # passado no formulario
+
+        talk.speaker = SpeakerUser.objects.get(
+            username=self.request.user.username
+        )
+        # associa o palestrante de acordo
+        # com uma pesquisa feito no model SpeakerUser
+        # filtrando pelo usuario logado
+
+        talk.save()
+        # salvar a o formulario
+
+        super(TalkCreateView, self).form_valid(form)
+        # chama o metodo super
+
+        # e retorna para a página de sucesso
+        return HttpResponseRedirect(
+            self.get_success_url()
+        )
+
+    def get_form_kwargs(self):
+        """
+        Retorna o usuário corrente para o formulario
+        """
+        kwargs = super(TalkCreateView, self).get_form_kwargs()
+        # kwargs['user'] = self.request.user
+        return kwargs
 
 
 class TalkListView(ListView):
