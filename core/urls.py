67edb71fa4d19336d05110306f5c8b98b036bc43
Lucas Simon <lucassrod@gmail.com>
2014-07-15 13:25:34 -0300

    Cria url para registro de conta

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,10 @@
 
 from django.conf.urls import patterns, url
 
-from .views import IndexHomePageTemplateView
+from .views import (
+    IndexHomePageTemplateView,
+    CustomRegisterCreateView
+)
 
 urlpatterns = patterns(
     'core.views',
@@ -10,5 +13,10 @@ urlpatterns = patterns(
         r'^$',
         IndexHomePageTemplateView.as_view(),
         name='home-page'
-    )
+    ),
+    url(
+        r"signup/$",
+        CustomRegisterCreateView.as_view(),
+        name="core-signup"
+    ),
 )
