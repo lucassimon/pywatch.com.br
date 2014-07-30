df4d20be20afc68a281f3cc5c28d02fce55b8bed
Lucas Simon <lucassrod@gmail.com>
2014-07-30 11:31:19 -0300

    Adiciona o mandrill as configuracoes de staging

--- pywatch/settings/staging.py
+++ pywatch/settings/staging.py
@@ -4,47 +4,49 @@ from decouple import ConfigIni
 import dj_database_url
 
 
-########## DEBUG CONFIGURATION
+# ######### DEBUG CONFIGURATION
 DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
 
-########## END DEBUG CONFIGURATION
+# ######### END DEBUG CONFIGURATION
 
 config = ConfigIni(PROJECT_DIR.child('confs')+'/settings-staging.ini')
-########## INSTALLED APPS CONFIGURATION
+# ######### INSTALLED APPS CONFIGURATION
 
 INSTALLED_APPS += (
     'django_nose',
     'nose',
     'gunicorn',
+    'djrill',
 )
 
-########## TEST SETTINGS
+# ######### TEST SETTINGS
 TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
 TEST_DISCOVER_TOP_LEVEL = PROJECT_DIR
 TEST_DISCOVER_ROOT = PROJECT_DIR
 TEST_DISCOVER_PATTERN = "test_*"
 NOSE_ARGS = ['--verbosity=2', '-x', '-d']
 os.environ['REUSE_DB'] = "1"
-########## END TEST SETTINGS
+# ######### END TEST SETTINGS
 
-########## DATABASE CONFIGURATION
+# ######### DATABASE CONFIGURATION
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_STAGING_URL')
     )
 }
-########## END DATABASE CONFIGURATION
+# ######### END DATABASE CONFIGURATION
 
-##########  MAILTRAP CONFIGURATION
-
+# #########  MAILTRAP CONFIGURATION
+EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+MANDRILL_API_KEY = config('EMAIL_HOST_PASSWORD')
 
-##########  END MAILTRAP CONFIGURATION
+# #########  END MAILTRAP CONFIGURATION
 
 ALLOWED_HOSTS = ['*']
