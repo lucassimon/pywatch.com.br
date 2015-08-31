64407ac225bee338e790a541e3307216f5622a7c
Lucas Simon <lucassrod@gmail.com>
2015-08-31 20:33:26 -0300

    Create ci wercker config

---
+++ pywatch/settings/ci-wercker.py
@@ -0,0 +1,39 @@
+import os
+from .base import *
+from decouple import ConfigIni
+import dj_database_url
+
+
+config = ConfigIni(PROJECT_DIR.child('confs')+'/settings.ini')
+########## INSTALLED APPS CONFIGURATION
+
+INSTALLED_APPS += (
+    'django_nose',
+    'nose',
+)
+
+########## TEST SETTINGS
+TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
+TEST_DISCOVER_TOP_LEVEL = PROJECT_DIR
+TEST_DISCOVER_ROOT = PROJECT_DIR
+TEST_DISCOVER_PATTERN = "test_*"
+NOSE_ARGS = ['--verbosity=2', '-x', '-d']
+os.environ['REUSE_DB'] = "1"
+########## END TEST SETTINGS
+
+########## DEBUG CONFIGURATION
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+COMPRESS_ENABLED = not DEBUG
+########## END DEBUG CONFIGURATION
+
+
+########## DATABASE CONFIGURATION
+DATABASES = {
+    'default': dj_database_url.config(
+        env="WERCKER_POSTGRESQL_URL"
+    )
+}
+########## END DATABASE CONFIGURATION
